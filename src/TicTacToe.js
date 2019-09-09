6e3cc503447321e9b8d4943683f6e43f3ac7ae2f
petterigit <33552590+petterigit@users.noreply.github.com>
2019-09-09 17:51:04 +0300

    kinda cool #1
    kinda not #1

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -15,6 +15,12 @@ var TicModel = function TicModel() {
   ];
 };
 
+TicModel.prototype.updateArray = function updateArray(index) {
+  let row = Math.floor(index / 5);
+  let column = index % 5;
+  this.ticArray[row][column] = this.marker;
+};
+
 TicModel.prototype.nextTurn = function nextTurn() {
   if (this.turn === 1) {
     this.marker = " X ";
@@ -23,6 +29,7 @@ TicModel.prototype.nextTurn = function nextTurn() {
     this.marker = " O ";
     this.turn = 1;
   }
+  //alert("next turn marker: " + this.marker);
 };
 
 /* VIEW
@@ -37,25 +44,33 @@ TicView.prototype.render = function render(ticModel) {
   let modelRows = ticModel.rows;
   let modelColumns = ticModel.columns;
   let modelMarker = ticModel.marker;
-
+  let modelArray = ticModel.ticArray;
   let htmlString = "";
   htmlString = htmlString + "<table>";
   for (let i = 0; i < modelRows; i++) {
     htmlString = htmlString + "<tr>";
     for (let j = 0; j < modelColumns; j++) {
       htmlString = htmlString + '<td><button id="ticCell">';
-      htmlString = htmlString + modelMarker;
+      if (modelArray[i][j] === null) {
+        htmlString = htmlString + " - ";
+      } else if (modelArray[i][j] === 1) {
+        htmlString = htmlString + " X ";
+      } else if (modelArray[i][j] === 0) {
+        htmlString = htmlString + " O ";
+      }
       htmlString = htmlString + "</button></td>";
     }
     htmlString = htmlString + "</tr>";
   }
   htmlString = htmlString + "</table>";
+
   this.element.innerHTML = htmlString;
 
   let ticCellButtons = this.element.querySelectorAll("#ticCell");
-
-  for (let i = 0; i < ticCellButtons.length; i++) {
-    ticCellButtons[i].addEventListener("click", this.updateView);
+  for (var i = 0; i < ticCellButtons.length; i++) {
+    ticCellButtons[i].addEventListener("click", function() {
+      this.updateView(i);
+    });
   }
 };
 
@@ -70,14 +85,15 @@ var TicController = function TicController(ticView, ticModel) {
 
 TicController.prototype.initialize = function initialize() {
   this.ticView.updateView = this.updateView.bind(this);
-
   this.ticView.render(this.ticModel);
   this.ticModel.nextTurn();
 };
 
-TicController.prototype.updateView = function updateView() {
-  this.ticView.render(this.ticModel);
+TicController.prototype.updateView = function updateView(index) {
+  alert("asd");
   this.ticModel.nextTurn();
+  this.ticModel.updateArray(index);
+  this.ticView.render(this.ticModel);
 };
 
 /* INITIALIZE
@@ -95,3 +111,4 @@ var controller = new TicController(ticView, ticModel);
  */
 
 controller.initialize();
+//controller.ticView.updateView(7);
